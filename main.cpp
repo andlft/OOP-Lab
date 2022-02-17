703a8e91d0f4ff599ca8f0f2750372b5f10d0dbe
andrei.lefter <92049917+andlft@users.noreply.github.com>
2022-02-17 10:36:56 +0200

    program nr perfecte

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, world!\n";
+using namespace std;
+int main()
+{
+    int a, b, aux;
+    cin >> a >> b;
+    cout<<"numerele perfecte sunt: "<< endl;
+    if(a>b)
+        {aux = a; a = b; b=aux;}
+    for(int i = a; i <= b; ++i)
+        {aux  =  0;
+        for(int j = 1; j<i; j++)
+            {
+            if (i%j==0)
+                aux += j;
+            }
+        if (aux == i)
+            cout<<i<<endl;
+        }
     return 0;
 }
