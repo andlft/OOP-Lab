9b9748ba45e4d75e4b4307b7e2627a95054af620
andrei.lefter <92049917+andlft@users.noreply.github.com>
2022-03-02 22:17:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,92 @@
 #include <iostream>
-#include <string.h>
-#include <cmath>
+using namespace std;
 
-
-
-class numar_complex{
-private:
-    double parte_reala;
-    double parte_imaginara;
+class Matrice{
+    int nr_linii, nr_coloane;
+    double matrice[1000][1000];
 
 public:
-    double get_parte_reala() const {
-        return parte_reala;
+    Matrice(double elem, int coloane, int linii){
+        nr_linii = linii;
+        nr_coloane = coloane;
+
+        for(int i = 0; i < linii; i++)
+            for(int j = 0; j<coloane; j++)
+                matrice[i][j] = elem;
     }
 
-    void set_parte_reala(double parte_reala_) {
-        numar_complex::parte_reala = parte_reala_;
-    }
-
-    double get_parte_imaginara() const {
-        return parte_imaginara;
-    }
-
-    void set_parte_imaginara(double parte_imaginara_) {
-        numar_complex::parte_imaginara = parte_imaginara_ ;
-    }
-
-
-public:
-    numar_complex(int parte_reala_, int parte_imaginara_){
-        parte_reala = parte_reala_;
-        parte_imaginara = parte_imaginara_;
-    }
-    ~numar_complex(){}
-
-    void afisare(numar_complex nr){
-        std::cout<<nr.parte_reala<<"+*"<<nr.parte_imaginara<<std::endl;
+    Matrice(int coloane, int linii){
+        nr_linii = linii;
+        nr_coloane = coloane;
 
     }
-    void afisare_modul (numar_complex nr){
-        std::cout<<sqrt(nr.parte_reala*nr.parte_reala+nr.parte_imaginara*nr.parte_imaginara)<<std::endl;
+
+    Matrice(Matrice const &matrix){
+        nr_linii = matrix.nr_linii;
+        nr_coloane = matrix.nr_coloane;
+
+        for (int i = 0; i < nr_linii; i++)
+            for(int j = 0; j < nr_coloane; j++)
+                matrice[i][j] = matrix.matrice[i][j];
+    }
+
+    friend istream& operator>>(istream& i, Matrice& matrix){
+        i>>matrix.nr_linii>>matrix.nr_coloane;
+        for(int k = 0; k < matrix.nr_linii; k++)
+            for(int j = 0; j < matrix.nr_coloane; j++)
+                i>>matrix.matrice[k][j];
+        return i;
+    }
+
+    friend ostream& operator<<(ostream& o, Matrice& matrix){
+        for(int i = 0; i < matrix.nr_linii; i++)
+            {
+            for(int j = 0; j < matrix.nr_coloane; j++)
+                o<<matrix.matrice[i][j]<<" ";
+            cout<<endl;
+            }
+        return o;
+    }
+
+    void operator=(Matrice matrix){
+        nr_linii = matrix.nr_linii;
+        nr_coloane = matrix.nr_coloane;
+
+        for (int i = 0; i < nr_linii; i++)
+            for(int j = 0; j < nr_coloane; j++)
+                matrice[i][j] = matrix.matrice[i][j];
+    }
+
+    void actualizare (double elem, int linii, int coloane){
+
+        for(int i = 0; i < linii; i++)
+            for(int j = 0; j<coloane; j++)
+                matrice[i][j] = elem;
+    }
+
+    friend Matrice operator+(Matrice matrix1, Matrice matrix2){
+        Matrice obiect(0, matrix1.nr_linii, matrix1.nr_coloane) ;
+        for(int i = 0; i < matrix1.nr_linii; i++)
+            for(int j = 0; j < matrix1.nr_coloane; j++)
+                obiect.matrice[i][j] = matrix1.matrice[i][j] + matrix2.matrice[i][j];
+        return obiect;
+    }
+
+    friend Matrice operator-(Matrice matrix1, Matrice matrix2){
+        Matrice obiect(0, matrix1.nr_linii, matrix1.nr_coloane) ;
+        for(int i = 0; i < matrix1.nr_linii; i++)
+            for(int j = 0; j < matrix1.nr_coloane; j++)
+                obiect.matrice[i][j] = matrix1.matrice[i][j] - matrix2.matrice[i][j];
+        return obiect;
+    }
+    // de continuat
+
+    friend int nrlinii(Matrice matrix){
+        return matrix.nr_linii;
 
     }
-    numar_complex operator+ (numar_complex nr1, numar_complex nr2){
-
-    }
+    // de continuat
 
 };
+
+
