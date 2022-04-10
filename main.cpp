8d341a8b70968c66d3877c4797bba2c5c926a005
andrei.lefter <92049917+andlft@users.noreply.github.com>
2022-04-10 19:49:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,115 +2,111 @@
 
 class Punct{
 private:
-    float *x = new float;
-    float *y = new float;
+    float x;
+    float y;
 public:
-    Punct(){   //constructor de initalizare fara parametrii
+    Punct() = default;
 
-    }
     Punct(float x_, float y_){    // constructor de initializare parametrizat
-        *x = x_;
-        *y = y_;
+        x = x_;
+        y = y_;
     }
 
     Punct(const Punct &pct){     // constructor de copiere
-        *x = *pct.x;
-        *y = *pct.y;
+        x = pct.x;
+        y = pct.y;
     }
-    virtual ~Punct(){     //destructor
-        delete x;
-        delete y;
-    }
+    virtual ~Punct() = default;
 
-    Punct operator= (const Punct &pct){
-        *x = *pct.x;
-        *y = *pct.y;
-        return *this;
-    }
+    Punct& operator= (const Punct &pct) = default;
 
     friend std::ostream& operator<< (std::ostream& o, const Punct &pct){
-        o<<*pct.x<<" "<<*pct.y;
+        o<<pct.x<<" "<<pct.y;
         return o;
     }
 
     friend std::istream& operator>> (std::istream& i, Punct &pct){
-        i>>*pct.x >>*pct.y;
+        i>>pct.x >>pct.y;
         return i;
     }
 
     void actualizare(const float& x_, const float& y_){
-        *x = x_;
-        *y = y_;
+        x = x_;
+        y = y_;
     }
 
-    float *getX() const {
+    float getX() const {
         return x;
     }
 
-    void setX(float *x_) {
-        Punct::x = x_;
+    void setX(float x_) {
+        x = x_;
     }
 
-    float *getY() const {
+    float getY() const {
         return y;
     }
 
-    void setY(float *y_) {
-        Punct::y = y_;
+    void setY(float y_) {
+        y = y_;
+    }
+
+    bool operator==(const Punct &rhs) const {
+        return x == rhs.x && y == rhs.y;
+    }
+
+    bool operator!=(const Punct &rhs) const {
+        return !(rhs == *this);
     }
 };
 
 class Patrat{
 protected:
-    Punct *pct_st_jos = new Punct;
-    float *latura = new float;
+    Punct pct_st_jos;
+    float latura;
 public:
-    Patrat(){  // constructor fara parametrii
+    Patrat() = default;
 
-    }
     Patrat(const float& x, const float& y,const float& latura_){  //constructor cu parametrii
-        pct_st_jos->actualizare(x,y);
-        *latura = latura_;
+        pct_st_jos.actualizare(x,y);
+        latura = latura_;
     }
 
     Patrat(const Patrat &patrat){ //constructor de copiere
-        *pct_st_jos = *patrat.pct_st_jos;
-        *latura = *patrat.latura;
+        pct_st_jos = patrat.pct_st_jos;
+        latura = patrat.latura;
     }
-    virtual ~Patrat(){  //destructor
-        delete pct_st_jos;
-        delete latura;
-    }
+    virtual ~Patrat() = default;
 
-    Patrat operator= (const Patrat& patrat_){
-        *pct_st_jos = *patrat_.pct_st_jos;
-        *latura = *patrat_.latura;
+    Patrat& operator= (const Patrat& patrat_){
+        pct_st_jos = patrat_.pct_st_jos;
+        latura = patrat_.latura;
         return *this;
     }
 
     friend std::ostream& operator<< (std::ostream& o, const Patrat &patrat_){
-        o<<*patrat_.pct_st_jos<<" "<<*patrat_.latura;
+        o<<patrat_.pct_st_jos<<" "<<patrat_.latura;
         return o;
     }
 
     friend std::istream& operator>> (std::istream& i, Patrat &patrat_){
-        i>>*patrat_.pct_st_jos >>*patrat_.latura;
+        i>>patrat_.pct_st_jos >>patrat_.latura;
         return i;
     }
 
-    Punct *getPctStJos() const {
+    Punct getPctStJos() const {
         return pct_st_jos;
     }
 
-    void setPctStJos(Punct *pctStJos) {
+    void setPctStJos(Punct pctStJos) {
         pct_st_jos = pctStJos;
     }
 
-    float *getLatura() const {
+    float getLatura() const {
         return latura;
     }
 
-    void setLatura(float *latura_) {
+    void setLatura(float latura_) {
         Patrat::latura = latura_;
     }
 
@@ -126,41 +122,38 @@ public:
 
 class Dreptunghi : virtual public Patrat{
 protected:
-    float *latura2 =  new float;
+    float latura2;
 public:
-    Dreptunghi(){ //constructor fara parametrii
-
-    }
+    Dreptunghi() = default;
 
     Dreptunghi(const float &x, const float &y, const float &latura,const float latura2_) :
     Patrat(x, y, latura) {
-        *latura2 = latura2_;
+        latura2 = latura2_;
     }
          //constructor de initializare cu parametrii
 
 
-    Dreptunghi(const Dreptunghi & dreptunghi_):Patrat(*dreptunghi_.pct_st_jos->getX(),
-                                                      *dreptunghi_.pct_st_jos->getY(),
-                                                      *dreptunghi_.latura)
+    Dreptunghi(const Dreptunghi & dreptunghi_):Patrat(dreptunghi_.pct_st_jos.getX(),
+                                                      dreptunghi_.pct_st_jos.getY(),
+                                                      dreptunghi_.latura)
                                                       { //constructor de copiere
-        *latura2 = *dreptunghi_.latura2;
+        latura2 = dreptunghi_.latura2;
     }
 
-    float *getLatura2() const {  //getter
+    float getLatura2() const {  //getter
         return latura2;
     }
 
-    void setLatura2(float *latura2_) { //setter
+    void setLatura2(float latura2_) { //setter
         Dreptunghi::latura2 = latura2_;
     }
 
-    virtual ~Dreptunghi() {   //destructor
-        delete latura2;
-    }
-    Dreptunghi operator = (const Dreptunghi &drptg){
-        *pct_st_jos = *drptg.pct_st_jos;
-        *latura = *drptg.latura;
-        *latura2 = *drptg.latura2;
+    virtual ~Dreptunghi() = default;
+
+    Dreptunghi& operator= (const Dreptunghi &drptg){
+        pct_st_jos = drptg.pct_st_jos;
+        latura = drptg.latura;
+        latura2 = drptg.latura2;
         return *this;
     }
 
@@ -178,42 +171,40 @@ public:
         return os;
     }
 
-    friend std::istream &operator>>(std::istream &is, const Dreptunghi &dreptunghi){
-        is >> *dreptunghi.pct_st_jos>>*dreptunghi.latura >> *dreptunghi.latura2;
+    friend std::istream &operator>>(std::istream &is, Dreptunghi &dreptunghi){
+        is >> dreptunghi.pct_st_jos>>dreptunghi.latura >> dreptunghi.latura2;
         return is;
     }
 };
 
 class Romb : virtual public Patrat{
 protected:
-    Punct *pct_colt_op = new Punct;
+    Punct pct_colt_op;
 public:
     Romb(){}//consturctor fara parametrii
 
     Romb(const float &x, const float &y, const float &latura, const float &z, const float&w) :
     Patrat(x, y, latura)  {
-        pct_colt_op->actualizare(z,w);
+        pct_colt_op.actualizare(z,w);
     }
     //constructor cu parametrii
 
-    Romb(const Romb &romb):Patrat(*romb.pct_st_jos->getX(), *romb.pct_st_jos->getY(), *romb.latura){
-        *pct_colt_op = *romb.pct_colt_op;
+    Romb(const Romb &romb):Patrat(romb.pct_st_jos.getX(), romb.pct_st_jos.getY(), romb.latura){
+        pct_colt_op = romb.pct_colt_op;
     }
 
 
     Punct getPctColtOp() const {
-        return *pct_colt_op;
+        return pct_colt_op;
     }
 
     void setPctColtOp(float x, float y) {
-        pct_colt_op->setX(&x);
-        pct_colt_op->setY(&y);
+        pct_colt_op.setX(x);
+        pct_colt_op.setY(y);
 
     }
 
-    virtual ~Romb() {
-        delete pct_colt_op;
-    }
+    virtual ~Romb() = default;
 
     bool operator==(const Romb &rhs) const {
         return static_cast<const Patrat &>(*this) == static_cast<const Patrat &>(rhs) &&
@@ -224,10 +215,10 @@ public:
         return !(rhs == *this);
     }
 
-    Romb operator= (const Romb &rmb){
-        *pct_st_jos = *rmb.pct_st_jos;
-        *pct_colt_op = *rmb.pct_colt_op;
-        *latura = *rmb.latura;
+    Romb& operator= (const Romb &rmb){
+        pct_st_jos = rmb.pct_st_jos;
+        pct_colt_op = rmb.pct_colt_op;
+        latura = rmb.latura;
         return *this;
     }
 
@@ -235,15 +226,15 @@ public:
         os << static_cast<const Patrat &>(romb) << romb.pct_colt_op;
         return os;
     }
-    friend std::istream &operator>>(std::istream &is, const Romb &romb) {
-        is >> *romb.pct_st_jos >> *romb.latura >> *romb.pct_colt_op;
+    friend std::istream &operator>>(std::istream &is, Romb &romb) {
+        is >> romb.pct_st_jos >> romb.latura >> romb.pct_colt_op;
         return is;
     }
 };
 
 class Paralelogram : public Dreptunghi, public Romb{
 public:
-    Paralelogram() {}
+    Paralelogram() = default;
 
             Paralelogram(
 
@@ -259,29 +250,33 @@ public:
     Romb(x, y, latura, x2, y2
     ){}
 
-    Paralelogram(const Paralelogram &prlgrm): Patrat(*prlgrm.pct_st_jos->getX(),
-                                                     *prlgrm.pct_st_jos->getY(),
-                                                     *prlgrm.latura),
-                                                        Dreptunghi(*prlgrm.pct_st_jos->getX(),
-                                                         *prlgrm.pct_st_jos->getY(),
-                                                         *prlgrm.latura,
-                                                         *prlgrm.latura2),
-                                                         Romb(*prlgrm.pct_st_jos->getX(),
-                                                              *prlgrm.pct_st_jos->getY(),
-                                                              *prlgrm.latura,
-                                                              *prlgrm.pct_colt_op->getX(),
-                                                              *prlgrm.pct_colt_op->getY()
+    Paralelogram(const Paralelogram &prlgrm): Patrat(prlgrm.pct_st_jos.getX(),
+                                                     prlgrm.pct_st_jos.getY(),
+                                                     prlgrm.latura),
+                                                        Dreptunghi(prlgrm.pct_st_jos.getX(),
+                                                         prlgrm.pct_st_jos.getY(),
+                                                         prlgrm.latura,
+                                                         prlgrm.latura2),
+                                                         Romb(prlgrm.pct_st_jos.getX(),
+                                                              prlgrm.pct_st_jos.getY(),
+                                                              prlgrm.latura,
+                                                              prlgrm.pct_colt_op.getX(),
+                                                              prlgrm.pct_colt_op.getY()
                                                          ){};
 
 
-    virtual ~Paralelogram() {
+    virtual ~Paralelogram() = default;
 
+    bool operator==(const Paralelogram &rhs) const {
+        return static_cast<const Romb &>(*this) == static_cast<const Romb &>(rhs) &&
+                static_cast<const Dreptunghi &>(*this) == static_cast<const Dreptunghi &>(rhs);
     }
-    Paralelogram operator= (const Paralelogram &prlgrm){
-        *pct_st_jos = *prlgrm.pct_st_jos;
-        *latura=*prlgrm.latura;
-        *latura2=*prlgrm.latura2;
-        *pct_colt_op = *prlgrm.pct_colt_op;
+
+    Paralelogram& operator= (const Paralelogram &prlgrm){
+        pct_st_jos = prlgrm.pct_st_jos;
+        latura=prlgrm.latura;
+        latura2=prlgrm.latura2;
+        pct_colt_op = prlgrm.pct_colt_op;
         return *this;
     }
 
@@ -290,24 +285,84 @@ public:
         return os;
     }
 
-    friend std::istream &operator>>(std::istream &is, const Paralelogram &paralelogram){
-        is >> *paralelogram.pct_st_jos
-        >>*paralelogram.latura
-        >>*paralelogram.latura2
-        >>*paralelogram.pct_colt_op;
+    friend std::istream &operator>>(std::istream &is, Paralelogram &paralelogram){
+        is >> paralelogram.pct_st_jos
+        >>paralelogram.latura
+        >>paralelogram.latura2
+        >>paralelogram.pct_colt_op;
         return is;
     }
 
 };
 
-//class Trapez : public Paralelogram{
+class Trapez : public Paralelogram{
+    float baza2;
+public:
+    Trapez() = default;
 
-//};
+    Trapez(const float &x,
+           const float &y,
+           const float &latura,
+           const float &latura2,
+           const float &x2,
+           const float &y2,
+           const float &baza2_
+           ): Paralelogram(x,y,latura,latura2, x2, y2)
+    {
+        baza2 = baza2_;
+    }
+
+    Trapez(const Trapez &trapez):Paralelogram(trapez.pct_st_jos.getX(),
+                                              trapez.pct_st_jos.getY(),
+                                              trapez.latura,
+                                              trapez.latura2,
+                                              trapez.pct_colt_op.getX(),
+                                              trapez.pct_colt_op.getY())
+    {
+        baza2 = trapez.baza2;
+    }
+
+    ~Trapez() = default;
+
+    float getBaza2() const {
+        return baza2;
+    }
+
+    void setBaza2(float baza2_) {
+        Trapez::baza2 = baza2_;
+    }
+    Trapez& operator= (const Trapez &trpz){
+        pct_st_jos = trpz.pct_st_jos;
+        latura = trpz.latura;
+        latura2 = trpz.latura2;
+        pct_colt_op = trpz.pct_colt_op;
+        return *this;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Trapez &trapez) {
+        os << static_cast<const Paralelogram &>(trapez) << " baza2: " << trapez.baza2;
+        return os;
+    }
+
+    friend std::istream &operator>>(std::istream &is, Trapez &trapez){
+        is >> trapez.pct_st_jos >> trapez.latura >> trapez.latura2 >> trapez.pct_colt_op >> trapez.baza2;
+        return is;
+    }
+
+    bool operator==(const Trapez &rhs) const {
+        return static_cast<const Paralelogram &>(*this) == static_cast<const Paralelogram &>(rhs) &&
+               baza2 == rhs.baza2;
+    }
+
+    bool operator!=(const Trapez &rhs) const {
+        return !(rhs == *this);
+    }
+};
 
 
 int main(){
     Punct pct1(20, 10);
-    Punct pct2 = pct1;
+    std::cout<<pct1;
     Punct pct3;
 
     Patrat patr(20, 10, 5);
@@ -336,3 +391,141 @@ int main(){
 //    std::cin>>pct1>>pct2;
 //    std::cout<<pct1<<std::endl<<pct2<<std::endl;
 }
+
+/*
+ * #include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+enum class PetType {
+    Dog,
+    Cat
+};
+
+class Pet {
+private:
+    std::string name;
+    int age;
+public:
+    const std::string &getName() const {
+        return name;
+    }
+
+    void setName(const std::string &name) {
+        Pet::name = name;
+    }
+
+    int getAge() const {
+        return age;
+    }
+
+    void setAge(int age) {
+        Pet::age = age;
+    }
+
+    Pet(std::string name, int age) : name(std::move(name)), age(age) {}
+
+    virtual std::string makeSound() const = 0;
+
+    virtual PetType getType() const = 0;
+
+    virtual void print(std::ostream &os) const = 0;
+
+    void sayHello() {
+        std::cout << makeSound() << " I am " << name << '\n';
+    }
+
+    virtual ~Pet() = default;
+
+    friend std::ostream &operator<<(std::ostream &os, const Pet *pet) {
+        pet->print(os);
+        return os;
+    }
+};
+
+class Dog : public Pet {
+public:
+    Dog(std::string name, int age) : Pet(std::move(name), age) {
+    }
+
+    Dog(const Dog& dog) : Pet(dog.getName(), dog.getAge()){ }
+
+    std::string makeSound() const override {
+        return "Woof!";
+    }
+
+    void print(std::ostream &os) const override {
+        os << "Dog " << getName() << '\n';
+    }
+
+    PetType getType() const override {
+        return PetType::Dog;
+    }
+
+    void fetch() const {
+        std::cout << "Dog fetched ball\n";
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Dog &dog) {
+        dog.print(os);
+        return os;
+    }
+};
+
+class Cat : public Pet {
+public:
+    Cat(const std::string &name, int age) : Pet(name, age) {}
+
+    std::string makeSound() const override {
+        return "Meow!";
+    }
+
+    void print(std::ostream &os) const override {
+        os << "Cat " << getName() << '\n';
+    }
+
+    PetType getType() const override {
+        return PetType::Cat;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Cat &cat) {
+        cat.print(os);
+        return os;
+    }
+};
+
+class PetShop {
+    static std::vector<std::shared_ptr<Pet>> pets;
+public:
+
+    static void addPet(const std::shared_ptr<Pet> &pet) {
+        pets.push_back(pet);
+    }
+
+    static std::vector<std::shared_ptr<Pet>> getPets() {
+        return pets;
+    }
+
+    static void showAllDogs() {
+        for (auto &pet: PetShop::getPets()) {
+            if (pet->getType() == PetType::Dog) {
+                auto dog = std::dynamic_pointer_cast<Dog>(pet);
+                dog->fetch();
+            }
+        }
+    }
+};
+
+std::vector<std::shared_ptr<Pet>> PetShop::pets;
+
+int main() {
+    auto dog = std::make_shared<Dog>("Scooby", 3);
+    auto cat = std::make_shared<Cat>("Kitty", 2);
+
+    PetShop::addPet(dog);
+    PetShop::addPet(cat);
+
+    PetShop::showAllDogs();
+}
+ */
