77ce3506119cc18e9299b73f75ddd61b5f908e3b
andrei.lefter <92049917+andlft@users.noreply.github.com>
2022-04-13 00:12:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-//#include <memory>
+#include <memory>
 #include <string>
 //#include <typeinfo>
 
@@ -95,7 +95,7 @@ public:
         i>>patrat_.pct_st_jos >>patrat_.latura;
         return i;
     }
-
+/*
 //    Punct getPctStJos() const {
 //        return pct_st_jos;
 //    }
@@ -111,7 +111,7 @@ public:
 //    void setLatura(float latura_) {
 //        Patrat::latura = latura_;
 //    }
-
+*/
     bool operator==(const Patrat &rhs) const {
         return pct_st_jos == rhs.pct_st_jos &&
                latura == rhs.latura;
@@ -240,6 +240,9 @@ public:
         is >> romb.pct_st_jos >> romb.latura >> romb.pct_colt_op;
         return is;
     }
+    virtual float arie(){
+        return latura * (pct_colt_op.getY()-pct_st_jos.getY());
+    }
 };
 
 class Paralelogram : public Dreptunghi, public Romb{
@@ -248,17 +251,19 @@ public:
 
             Paralelogram(
 
-    const float &x,
-    const float &y,
-    const float &latura,
-    const float &latura2,
-    const float &x2,
-    const float &y2
-    ):
-    Dreptunghi(x, y, latura, latura2
-    ),
-    Romb(x, y, latura, x2, y2
-    ){}
+    const float &x_,
+    const float &y_,
+    const float &latura_,
+    const float &latura2_,
+    const float &x2_,
+    const float &y2_
+    )
+    {
+                pct_st_jos.actualizare(x_,y_);
+                latura = latura_;
+                latura2=latura2_;
+                pct_colt_op.actualizare(x2_,y2_);
+            }
 
     Paralelogram(const Paralelogram &prlgrm): Patrat(prlgrm.pct_st_jos.getX(),
                                                      prlgrm.pct_st_jos.getY(),
@@ -290,8 +295,8 @@ public:
         return *this;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const Paralelogram &paralelogram) {
-        os << static_cast<const Dreptunghi &>(paralelogram) <<static_cast<const Romb &>(paralelogram);
+    friend std::ostream &operator<<(std::ostream &os, const Paralelogram &para) {
+        os << para.pct_st_jos<<" "<<para.latura<<" "<<para.latura2<<" "<<para.pct_colt_op;
         return os;
     }
 
@@ -301,6 +306,9 @@ public:
         >>paralelogram.latura2
         >>paralelogram.pct_colt_op;
         return is;
+    }
+    virtual float arie(){
+        return latura * (pct_st_jos.getY()-pct_colt_op.getY());
     }
 
 };
@@ -356,7 +364,7 @@ public:
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Trapez &trapez) {
-        os << static_cast<const Paralelogram &>(trapez) << " baza2: " << trapez.baza2;
+        os << static_cast<const Paralelogram &>(trapez) << " "<< trapez.baza2;
         return os;
     }
 
@@ -373,100 +381,105 @@ public:
     bool operator!=(const Trapez &rhs) const {
         return !(rhs == *this);
     }
+    virtual float arie(){
+        return (latura+baza2)/2 * (pct_st_jos.getY()-pct_colt_op.getY());
+    }
 };
 
-//class Shapes {
-//    static std::vector<std::shared_ptr<Patrat>> shapes;
-//public:
-//    static void addShape (const std::shared_ptr<Patrat> &shp){
-//        shapes.push_back(shp);
-//    }
-//    static std::vector<std::shared_ptr<Patrat>> getShapes(){
-//        return shapes;
-//    }
-//
-//    static void showPatrate(){
-//        for (int i = 0; i < static_cast<int>(getShapes().size()); i++){
-//            auto p= std::dynamic_pointer_cast<Dreptunghi>(shapes[i]);
-//            if(p != nullptr){
-//                    std::cout<<"patrat: "<<*p<<"\n";
-//            }
-//        }
-//    }
-//};
-
-//std::vector <std::shared_ptr<Patrat>> Shapes::shapes;
-
-class Shapes{
-    static std::vector<Patrat*> shapes;
+class Shapes {
+    static std::vector<std::shared_ptr<Patrat>> shapes;
 public:
-    static void addShape (Patrat &patrat){
-        shapes.push_back(&patrat);
+    static void addShape (const std::shared_ptr<Patrat> &shp){
+        shapes.push_back(shp);
     }
-    static void deleteShape (int i){
+    static std::vector<std::shared_ptr<Patrat>> getShapes(){
+        return shapes;
+    }
+        static void deleteShape (int i){
         shapes.erase(shapes.begin()+i);
     }
-    static void showShapes(std::string type){
-        for (int i = 0; i < static_cast <int> (shapes.size()); i++ ){
-
-            if(type == typeid(Patrat).name()){
-                Patrat *d = dynamic_cast<Patrat*>(shapes[i]);
-                if(d!= nullptr) {
-                    std::cout << "patrat: " << *d << "\n";
-                }
-            }
-            if(type == typeid(Dreptunghi).name()) {
-                Dreptunghi *d = dynamic_cast<Dreptunghi *>(shapes[i]);
-                if (d != nullptr){
-                std::cout << "dreptunghi: " << *d << "\n";
-               }
-            }
-            if(type == typeid(Romb).name()) {
-                Romb *d = dynamic_cast<Romb *>(shapes[i]);
-                if(d!= nullptr){
-                std::cout << "romb: " << *d << "\n";
-                }
-            }
-            if(type == typeid(Paralelogram).name()) {
-                Paralelogram *d = dynamic_cast<Paralelogram *>(shapes[i]);
-                if(d != nullptr){
-                std::cout << "paralelogram: " << *d << "\n";
-                }
-            }
-            if(type == typeid(Trapez).name()) {
-                Trapez *d = dynamic_cast<Trapez *>(shapes[i]);
-                if(d != nullptr){
-                std::cout << "trapez: " << *d << "\n";
-                }
-            }
-        }
+    static float showArea(int index){
+        return shapes[index]->arie();
     }
 
-    static void showAllShapes(){
+    static void showShapes(std::string type){
+        for (int i = 0; i < static_cast<int>(getShapes().size()); i++){
+
+            auto d1 = std::dynamic_pointer_cast<Trapez>(shapes[i]);
+            if(d1!= nullptr){
+                if(type == typeid(Trapez).name())
+                {
+                    std::cout<<"Trapez: "<< *d1<<"\n";
+                    continue;
+                }
+                else continue;
+            }
+            auto d2 = std::dynamic_pointer_cast<Paralelogram>(shapes[i]);
+            if(d2!= nullptr){
+                if(type == typeid(Paralelogram).name())
+                {
+                    std::cout<<"Paralelogram: "<< *d2<<"\n";
+                    continue;
+                }
+                else continue;
+            }
+            auto d3 = std::dynamic_pointer_cast<Romb>(shapes[i]);
+            if(d3!=nullptr){
+                if(type == typeid(Romb).name())
+                 {
+                    std::cout<<"Romb: "<< *d3<<"\n";
+                    continue;
+                }
+                else continue;
+            }
+
+            auto d4 = std::dynamic_pointer_cast<Dreptunghi>(shapes[i]);
+            if(d4!=nullptr){
+                if(type == typeid(Dreptunghi).name())
+                 {
+                    std::cout<<"Dreptunghi: "<< *d4<<"\n";
+                    continue;
+                }
+                else continue;
+            }
+
+            auto d5 = std::dynamic_pointer_cast<Patrat>(shapes[i]);
+            if(d5!=nullptr) {
+                if(type == typeid(Patrat).name())
+                {
+                    std::cout<<"Patrat: "<< *d5<<"\n";
+                    continue;
+                }
+                else continue;
+            }
+
+            }
+        }
+        static void showAllShapes(){
         for (int i = 0; i < static_cast <int> (shapes.size()); i++ ){
             std::cout<<i<<": ";
-            Trapez *d5 = dynamic_cast<Trapez *>(shapes[i]);
+            auto d5 = std::dynamic_pointer_cast<Trapez>(shapes[i]);
             if(d5 != nullptr){
                 std::cout << "trapez: " << *d5 << "\n";
                 continue;
             }
-            Paralelogram *d4 = dynamic_cast<Paralelogram *>(shapes[i]);
+            auto d4 = std::dynamic_pointer_cast<Paralelogram>(shapes[i]);
             if(d4 != nullptr){
                 std::cout << "paralelogram: " << *d4 << "\n";
                 continue;
             }
-            Romb *d3 = dynamic_cast<Romb *>(shapes[i]);
+            auto d3 = std::dynamic_pointer_cast<Romb>(shapes[i]);
             if(d3!= nullptr){
                 std::cout << "romb: " << *d3 << "\n";
                 continue;
             }
 
-            Dreptunghi *d2 = dynamic_cast<Dreptunghi *>(shapes[i]);
+            auto d2 = std::dynamic_pointer_cast<Dreptunghi>(shapes[i]);
             if (d2 != nullptr){
                 std::cout << "dreptunghi: " << *d2 << "\n";
                 continue;
             }
-            Patrat *d1 = dynamic_cast<Patrat*>(shapes[i]);
+            auto d1 = dynamic_pointer_cast<Patrat>(shapes[i]);
             if(d1!= nullptr) {
                 std::cout << "patrat: " << *d1<< "\n";
                 continue;
@@ -476,28 +489,253 @@ public:
     }
 };
 
-std:: vector <Patrat*> Shapes::shapes;
+std::vector <std::shared_ptr<Patrat>> Shapes::shapes;
 
+/*
+//class Shapes{
+//    static std::vector<Patrat*> shapes;
+//public:
+//    static void addShape (Patrat &patrat){
+//        shapes.push_back(&patrat);
+//    }
+//    static void deleteShape (int i){
+//        shapes.erase(shapes.begin()+i);
+//    }
+//    static void showShapes(std::string type){
+//        for (int i = 0; i < static_cast <int> (shapes.size()); i++ ){
+//
+//            if(type == typeid(Patrat).name()){
+//                Patrat *d = dynamic_cast<Patrat*>(shapes[i]);
+//                if(d!= nullptr) {
+//                    std::cout << "patrat: " << *d << "\n";
+//                }
+//            }
+//            if(type == typeid(Dreptunghi).name()) {
+//                Dreptunghi *d = dynamic_cast<Dreptunghi *>(shapes[i]);
+//                if (d != nullptr){
+//                std::cout << "dreptunghi: " << *d << "\n";
+//               }
+//            }
+//            if(type == typeid(Romb).name()) {
+//                Romb *d = dynamic_cast<Romb *>(shapes[i]);
+//                if(d!= nullptr){
+//                std::cout << "romb: " << *d << "\n";
+//                }
+//            }
+//            if(type == typeid(Paralelogram).name()) {
+//                Paralelogram *d = dynamic_cast<Paralelogram *>(shapes[i]);
+//                if(d != nullptr){
+//                std::cout << "paralelogram: " << *d << "\n";
+//                }
+//            }
+//            if(type == typeid(Trapez).name()) {
+//                Trapez *d = dynamic_cast<Trapez *>(shapes[i]);
+//                if(d != nullptr){
+//                std::cout << "trapez: " << *d << "\n";
+//                }
+//            }
+//        }
+//    }
+//
+//    static void showAllShapes(){
+//        for (int i = 0; i < static_cast <int> (shapes.size()); i++ ){
+//            std::cout<<i<<": ";
+//            Trapez *d5 = dynamic_cast<Trapez *>(shapes[i]);
+//            if(d5 != nullptr){
+//                std::cout << "trapez: " << *d5 << "\n";
+//                continue;
+//            }
+//            Paralelogram *d4 = dynamic_cast<Paralelogram *>(shapes[i]);
+//            if(d4 != nullptr){
+//                std::cout << "paralelogram: " << *d4 << "\n";
+//                continue;
+//            }
+//            Romb *d3 = dynamic_cast<Romb *>(shapes[i]);
+//            if(d3!= nullptr){
+//                std::cout << "romb: " << *d3 << "\n";
+//                continue;
+//            }
+//
+//            Dreptunghi *d2 = dynamic_cast<Dreptunghi *>(shapes[i]);
+//            if (d2 != nullptr){
+//                std::cout << "dreptunghi: " << *d2 << "\n";
+//                continue;
+//            }
+//            Patrat *d1 = dynamic_cast<Patrat*>(shapes[i]);
+//            if(d1!= nullptr) {
+//                std::cout << "patrat: " << *d1<< "\n";
+//                continue;
+//            }
+//
+//        }
+//    }
+//};
+//
+//std:: vector <Patrat*> Shapes::shapes;
+ */
+
+class Menu{
+    int option = -1, index = 0;
+public:
+    void showMenu(){
+        std::cout<<"Alege optiunea pe care o doresti:\n"
+        <<"1.Adauga o forma\n"
+        <<"2.Sterge o forma\n"
+        <<"3.Arata toate formele\n"
+        <<"4.Arata formele de un anumit tip\n"
+        <<"5.Calculeaza aria unei forme\n"
+        <<"0.Iesire din program\n"
+        <<"Numar optiune: ";
+        std::cin>>option;
+        std::cout<<"-----------------------------------------------\n";
+    }
+    void showAvailableShapes(){
+        std::cout<<"Ce forma doresti?\n"
+        <<"1.Patrat\n"
+        <<"2.Dreptunghi\n"
+        <<"3.Romb\n"
+        <<"4.Paralelogram\n"
+        <<"5.Trapez\n"
+        <<"Numar forma:";
+        std::cin>>index;
+        std::cout<<"-----------------------------------------------\n";
+    }
+    void showIndexQuestion(){
+        std::cout<<"Da indexul formei:\n";
+        std::cin>>index;
+    }
+
+    void addShape(){
+    showAvailableShapes();
+        if (index==1){
+            float x, y, z;
+            std::cout<<"Da x si y punct si dimensiunea laturii:\n";
+            std::cin>>x>>y>>z;
+            auto p = std::make_shared<Patrat>(Patrat(x,y,z));
+            Shapes::addShape(p);}
+
+        if (index==2){
+            float x, y, z, w;
+            std::cout<<"Da x si y punct si dimensiunile laturilor 1 si 2\n";
+            std::cin>>x>>y>>z>>w;
+            auto p = std::make_shared<Dreptunghi>(Dreptunghi(x,y,z,w));
+            Shapes::addShape(p);
+        }
+        if (index==3){
+            float x, y, z, w, u;
+            std::cout<<"Da x si y punct si dimensiunea laturii si x si y pentru punctul opus\n";
+            std::cin>>x>>y>>z>>w>>u;
+            auto p = std::make_shared<Romb>(Romb(x,y,z,w,u));
+            Shapes::addShape(p);
+        }
+        if (index==4){
+            float x, y, z, w, u, r;
+            std::cout<<"Da x si y punct, dimensiunea laturii1 si laturii2 si x si y pentru punctul opus\n";
+            std::cin>>x>>y>>z>>w>>u>>r;
+            auto p = std::make_shared<Paralelogram>(Paralelogram(x,y,z,w,u,r));
+            Shapes::addShape(p);
+        }
+        if(index==5){
+            float x, y, z, w, u, r, t;
+            std::cout<<"Da x si y punct, dimensiunea laturii1 si laturii2 si x si y pentru punctul opus si baza a 2 a\n";
+            std::cin>>x>>y>>z>>w>>u>>r>>t;
+            Trapez trpz(x,y,z,w,u,r,t);
+            auto p = std::make_shared<Trapez>(x,y,z,w,u,r,t);
+            Shapes::addShape(p);
+        }
+    }
+
+    void deleteShape(){
+        showIndexQuestion();
+        Shapes::deleteShape(index);
+    }
+    void showShape(){
+        showAvailableShapes();
+        if(index==1){
+            Shapes::showShapes(typeid(Patrat).name());
+        }
+        if(index==2){
+            Shapes::showShapes(typeid(Dreptunghi).name());
+        }
+        if(index==3){
+            Shapes::showShapes(typeid(Romb).name());
+        }
+        if(index==4){
+            Shapes::showShapes(typeid(Paralelogram).name());
+        }
+        if(index==5){
+            Shapes::showShapes(typeid(Trapez).name());
+        }
+        std::cout<<"-----------------------------------------------\n";
+    }
+
+    void showArea(){
+        showIndexQuestion();
+        std::cout<<"Aria este: "<<Shapes::showArea(index)<<"\n"
+        <<"--------------------------------------------------"<<"\n";
+
+    }
+
+    void runMenu(){
+        while(option!=0){
+            showMenu();
+            if(option == 1){
+                addShape();
+            }
+            if(option == 2){
+                deleteShape();
+            }
+            if(option == 3){
+                Shapes::showAllShapes();
+                std::cout<<"-----------------------------------------------\n";
+            }
+            if(option == 4){
+                showShape();
+            }
+            if(option == 5){
+                showArea();
+            }
+
+        }
+    }
+};
 
 int main(){
-
+    Patrat patrat(2,3,4);
+    std::cout<<patrat.arie()<<std::endl;
+    Romb romb(1,2,3,4,5);
+    std::cout<<romb.arie()<<std::endl;
 //    auto patrat = std::make_shared<Patrat>(3, 5, 4);
 //    auto drept = std::make_shared<Dreptunghi>(3, 5, 6, 5);
+//    auto trpz = std::make_shared<Trapez>(1,2,3,4,5,6,7);
+//    auto patrat = std::make_shared<Patrat>(1,2,3);
+//    auto romb = std::make_shared<Romb>(1,2,3,46,6);
+//    auto drpt = std::make_shared<Dreptunghi>(1,2,3,4);
+//    auto prlg =std::make_shared<Paralelogram>(1,2,3,4,5,6);
+//    Shapes::addShape(patrat);
+//    Shapes::addShape(drpt);
+//    Shapes::addShape(romb);
+//    Shapes::addShape(trpz);
+//    Shapes::addShape(prlg);
+//    Shapes::showAllShapes();
+//    Paralelogram pr(1, 2, 3, 4 ,5, 8);
+//    std::cout<<pr;
+////Patrat patrat (3,4,5);
+////Patrat patrat2 (10, 20, 30);
+////Dreptunghi drept (7, 8, 9, 10);
+//    Shapes::addShape(patrat);
+//    Shapes::addShape(drept);
+//    Shapes::showAllShapes();
+//    Shapes::deleteShape(1);
+    Menu meniu;
+    meniu.Menu::runMenu();
 
-Patrat patrat (3,4,5);
-Patrat patrat2 (10, 20, 30);
-Dreptunghi drept (7, 8, 9, 10);
-    Shapes::addShape(patrat);
-    Shapes::addShape(drept);
-    Shapes::showAllShapes();
-    Shapes::deleteShape(1);
 
 
 
-
-//    Shapes::showShapes(typeid(Dreptunghi).name());
-    std::cout<<"-----------------------"<<std::endl;
-    Shapes::showAllShapes();
+////    Shapes::showShapes(typeid(Dreptunghi).name());
+//    std::cout<<"-----------------------"<<std::endl;
+//    Shapes::showAllShapes();
     return 0;
 //    Punct pct1(20, 10);
 //    std::cout<<pct1;
